aaa5e3ecf5848d1a00f5d8b2f2695d54e04288e5
KrishnanAshwin <71191995+KrishnanAshwin@users.noreply.github.com>
2020-09-12 14:29:58 -0500

    Delete test_utils.py

--- test_utils.py
+++
@@ -1,8 +0,0 @@
-def test_find_plan():
-    """
-    Prints message if assertion fails.
-    """
-    try:
-        assert find_plan('AGG', ['8D8D','5QR3AGG','DEEE','ASDAD','asdaAGG']) == ['5QR3AGG']
-    except AssertionError:
-        print("Assertion error: The find_plan values are not asserting properly")
